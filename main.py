44a6572206017ad325b90bfa049842353eea001a
ZivBenda <34305811+ZivBenda@users.noreply.github.com>
2023-02-23 19:43:48 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import shap
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+from sklearn.preprocessing import LabelEncoder
 from sklearn import neighbors
 from sklearn import svm as skl_svm
 from sklearn.model_selection import train_test_split
@@ -18,13 +20,14 @@ IRIS_URL = "https://raw.githubusercontent.com/ZivBenda/tabular_data_science_biu_
 TITANIC_URL = "https://raw.githubusercontent.com/ZivBenda/tabular_data_science_biu_final_project/main/datasets/titanic.csv"
 HOUSES_URL = "https://raw.githubusercontent.com/ZivBenda/tabular_data_science_biu_final_project/main/datasets/houses.csv"
 CANCER_URL = "https://raw.githubusercontent.com/ZivBenda/tabular_data_science_biu_final_project/main/datasets/cancer.csv"
+SLEEPING_URL = "https://raw.githubusercontent.com/ZivBenda/tabular_data_science_biu_final_project/main/datasets/sleeping.csv"
 
 
 def reload_datasets():
-    return {"iris_dataset": pd.read_csv(IRIS_URL),
-            "titanic_dataset": pd.read_csv(TITANIC_URL),
-            "houses_dataset": pd.read_csv(HOUSES_URL),
-            "cancer_dataset": pd.read_csv(CANCER_URL)
+    return {  # "iris_dataset": pd.read_csv(IRIS_URL),
+              # "titanic_dataset": pd.read_csv(TITANIC_URL),
+            "sleeping_dataset": pd.read_csv(SLEEPING_URL),
+              #  "cancer_dataset": pd.read_csv(CANCER_URL)
             }
 
 
@@ -40,41 +43,36 @@ def preprocess_wine_dataset(df):
 
 def preprocess_dataset(name, dataset):
     if name == "iris_dataset":
-        # dataset['species'] = dataset['species'].astype(int)
+        dataset['species'] = LabelEncoder().fit_transform(dataset['species'])
+        dataset['species'] = dataset['species'].astype(int)
+        dataset = shuffle(dataset)
         Y = dataset['species']
         X = dataset[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
     elif name == "titanic_dataset":
+        dataset['Sex'] = LabelEncoder().fit_transform(dataset['Sex'])
+        dataset['Ticket'] = LabelEncoder().fit_transform(dataset['Ticket'])
+        dataset['Cabin'] = LabelEncoder().fit_transform(dataset['Cabin'])
+        dataset['Embarked'] = LabelEncoder().fit_transform(dataset['Embarked'])
         dataset['Survived'] = dataset['Survived'].astype(int)
+        dataset["Age"] = dataset["Age"].fillna(dataset["Age"].mean())
         Y = dataset['Survived']
         X = dataset[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']]
-    elif name == "houses_dataset":
-        dataset['SalePrice'] = dataset['SalePrice'].astype(int)
-        Y = dataset['SalePrice']
-        X = dataset[['MSSubClass', 'MSZoning', 'LotFrontage', 'LotArea', 'Street',
-                     'Alley', 'LotShape', 'LandContour', 'Utilities', 'LotConfig',
-                     'LandSlope', 'Neighborhood', 'Condition1', 'Condition2', 'BldgType',
-                     'HouseStyle', 'OverallQual', 'OverallCond', 'YearBuilt', 'YearRemodAdd',
-                     'RoofStyle', 'RoofMatl', 'Exterior1st', 'Exterior2nd', 'MasVnrType',
-                     'MasVnrArea', 'ExterQual', 'ExterCond', 'Foundation', 'BsmtQual',
-                     'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinSF1',
-                     'BsmtFinType2', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', 'Heating',
-                     'HeatingQC', 'CentralAir', 'Electrical', '1stFlrSF', '2ndFlrSF',
-                     'LowQualFinSF', 'GrLivArea', 'BsmtFullBath', 'BsmtHalfBath', 'FullBath',
-                     'HalfBath', 'BedroomAbvGr', 'KitchenAbvGr', 'KitchenQual',
-                     'TotRmsAbvGrd', 'Functional', 'Fireplaces', 'FireplaceQu', 'GarageType',
-                     'GarageYrBlt', 'GarageFinish', 'GarageCars', 'GarageArea', 'GarageQual',
-                     'GarageCond', 'PavedDrive', 'WoodDeckSF', 'OpenPorchSF', 'EnclosedPorch',
-                     '3SsnPorch', 'ScreenPorch', 'PoolArea', 'PoolQC', 'Fence', 'MiscFeature',
-                     'MiscVal', 'MoSold', 'YrSold', 'SaleType', 'SaleCondition']]
+    elif name == "sleeping_dataset":
+        dataset['sl'] = dataset['sl'].astype(int)
+        Y = dataset['sl']
+        X = dataset[['sr', 'rr', 't', 'lm', 'bo', 'rem', 'sr', 'hr']]
     elif name == "cancer_dataset":
-        dataset['diagnosis'] = dataset['diagnosis'].astype(int)
+        dataset['diagnosis'] = LabelEncoder().fit_transform(dataset['diagnosis'])
         Y = dataset['diagnosis']
-        X = dataset.drop([['diagnosis', 'id']])
-        """
         X = dataset[['radius_mean', 'texture_mean', 'perimeter_mean', 'area_mean',
-                     'smoothness_mean', 'compactness_mean', 'concavity_mean', 'concave points_mean',
-                     'symmetry_mean', 'fractal_dimension_mean', 'radius_se']]
-        """
+                     'smoothness_mean', 'compactness_mean', 'concavity_mean',
+                     'concave points_mean', 'symmetry_mean', 'fractal_dimension_mean',
+                     'radius_se', 'texture_se', 'perimeter_se', 'area_se', 'smoothness_se',
+                     'compactness_se', 'concavity_se', 'concave points_se', 'symmetry_se',
+                     'fractal_dimension_se', 'radius_worst', 'texture_worst',
+                     'perimeter_worst', 'area_worst', 'smoothness_worst',
+                     'compactness_worst', 'concavity_worst', 'concave points_worst',
+                     'symmetry_worst', 'fractal_dimension_worst']]
     elif name == "wine_dataset":
         df['quality'] = df['quality'].astype(int)
         Y = df['quality']
